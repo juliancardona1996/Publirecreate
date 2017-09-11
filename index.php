065be747c592097ea5149c49f204a06e420b9dbb
juliancardona1996 <juliancardona1996@gmail.com>
2017-09-11 02:07:39 -0500

    Nuevos cambios

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-fsfnsionfosbnfio
+<?php 
+
+$view = isset($_GET['view']) ? $_GET['view'] : 'index';
+
+if(file_exists('controllers/'.$view.'Controller.php')) {
+	include('controllers/'.$view.'Controller.php');
+}else{
+
+	//Error si no existe la página
+	include('controllers/indexController.php');
+}
+
+?>
